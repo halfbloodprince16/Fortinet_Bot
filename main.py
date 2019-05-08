ea9a6501d6b3e8a3a5ddf794884b5a8aa8eb9807
Vighnesh Tiwari <26187718+halfbloodprince16@users.noreply.github.com>
2019-05-08 21:52:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+usernameStr = 'username'
+passwordStr = 'password'
+
+browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+browser.get('https://www.hackerrank.com/auth/login')
+
+emailInput = browser.find_elements_by_css_selector('form input')[0]
+passwordInput = browser.find_elements_by_css_selector('form input')[1]
+
+emailInput.send_keys(usernameStr)
+passwordInput.send_keys(passwordStr)
+passwordInput.send_keys(Keys.ENTER)
